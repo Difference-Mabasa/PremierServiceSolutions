f8eb188f670cb2bbd7fef34de46f3d69c53ac2a6
Covenant Nethengwe <covenantn9@gmail.com>
2021-04-19 16:02:12 +0200

    Adding class IndividualClientJobsBLL in the BusinessLayer and implementing INSERT, UPDATE and GET methods

---
+++ PremierServiceSolutions/BusinessLogicLayer/IndividualClientJobsBLL.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PremierServiceSolutions.DataAccessLayer;
+
+namespace PremierServiceSolutions.BusinessLogicLayer
+{
+    class IndividualClientJobsBLL
+    {
+        public List<Job> GetIndividualClientJobByClientID(string id)
+        {
+            List<Job> jobs = new List<Job>();
+            try
+            {
+                IndividualClientJobsDAL clientData = new IndividualClientJobsDAL();
+                jobs = clientData.GetIndividualClientJobByClientID(id);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show($"Error: {e.Message}");
+            }
+
+            return jobs;
+        }
+
+        public void InsertIndividualClientJob
+    }
+}
