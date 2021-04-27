7f42ac58ccf0796ae562933174986bd39c2eb3a3
Kamohelo <576658@student.belgiumcampus.ac.za>
2021-04-27 02:23:41 +0200

    Added Insert Method for Employee

--- PremierServiceSolutions/DataAccessLayer/EmployeeDAL.cs
+++ PremierServiceSolutions/DataAccessLayer/EmployeeDAL.cs
@@ -1,8 +1,46 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Windows.Forms;
 
-public class Class1
+namespace PremierServiceSolutions.DataAccessLayer
 {
-	public Class1()
-	{
-	}
+
+    class EmployeeDAL
+    {
+        SqlConnection conn = new SqlConnection("Server= (local); Database = PremierServiceSolutionsDB; Trusted_Connection = true");
+        SqlCommand cmd;
+        string query;
+        public void InsertEmployee(Employee emp)
+        {
+            string query = $"insert into Employees(EmployeeID,EmployeeName,EmployeeSurname,Department,JobTitle,Phone,Email,Password) values" +
+            $"('{emp.EmployeeID}', " +
+            $"'{emp.Name}', " +
+            $"'{emp.Surname}', " +
+            $"'{emp.Department}', " +
+            $"'{emp.JobTitle}', " +
+            $"'{emp.Phone}', " +
+            $"'{emp.Email}', " +
+            $"'{emp.Password}')";
+
+            try
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("ERROR: " + ex.Message);
+            }
+
+            finally
+            {
+                conn.Close();
+            }
+        }
+    }
 }
