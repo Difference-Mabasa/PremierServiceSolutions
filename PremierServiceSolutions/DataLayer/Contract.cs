0311fa8f8498f19b8ce58721163636aea9e3e01a
Covenant Nethengwe <covenantn9@gmail.com>
2021-04-28 18:40:22 +0200

    Moving service level field from Indi/BusiClient to Contract class

--- PremierServiceSolutions/DataLayer/Contract.cs
+++ PremierServiceSolutions/DataLayer/Contract.cs
@@ -8,12 +8,12 @@ namespace PremierServiceSolutions
 {
     abstract class Contract
     {
-        string contractId;
+        string contractId, serviceLevel;
         double price;
 
         public string ContractId { get => contractId; set => contractId = value; }
+        public string ServiceLevel { get => serviceLevel; set => serviceLevel = value; } 
         public abstract double Price { get ; set; }
-
         //anything
 
 
