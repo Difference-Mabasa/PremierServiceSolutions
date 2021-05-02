e756122e8cfd9029d904eff8a4da2b019955430e
il-sea7 <itu.kev.lesu@gmail.com>
2021-05-02 16:43:21 +0200

    try

--- PremierServiceSolutions/PresentationLayer/Form1.cs
+++ PremierServiceSolutions/PresentationLayer/Form1.cs
@@ -17,6 +17,7 @@ namespace PremierServiceSolutions.PresentationLayer
         public Form1()
         {
             InitializeComponent();
+            //try
         }
 
         private void btnLogin_Click(object sender, EventArgs e)
