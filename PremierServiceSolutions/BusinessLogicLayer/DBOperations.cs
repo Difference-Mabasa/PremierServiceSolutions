2db99b82a0b268c272ac3dc1ad99fdf97ac870ad
Covenant Nethengwe <covenantn9@gmail.com>
2021-05-03 11:12:14 +0200

    Implementing Allclients() in the DBOperations

--- PremierServiceSolutions/BusinessLogicLayer/DBOperations.cs
+++ PremierServiceSolutions/BusinessLogicLayer/DBOperations.cs
@@ -29,5 +29,19 @@ namespace PremierServiceSolutions.BusinessLogicLayer
                 MessageBox.Show("Unable To assign Technician");
             }
         }
+
+        public List<Object> AllClients()
+        {
+            List<Object> obj = new List<Object>();
+            IndividualClientDAL indiData = new IndividualClientDAL();
+            BusinessClientDAL busiData = new BusinessClientDAL();
+            List<IndividualClient> individual = indiData.GetAllIndividualClients();
+            List<BusinessClient> business = busiData.GetAllBusinessClients();
+            obj.Add(individual);
+            obj.Add(business);
+            return obj;
+
+        }
+
     }
 }
