dcb3814e71d14c324d840df67ac957dcf50bcd42
Kamohelo <576658@student.belgiumcampus.ac.za>
2021-05-05 20:28:22 +0200

    Created GetAllIndividualClientCallReports method

--- PremierServiceSolutions/BusinessLogicLayer/IndividualClientCallReportsBLL.cs
+++ PremierServiceSolutions/BusinessLogicLayer/IndividualClientCallReportsBLL.cs
@@ -3,10 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using PremierServiceSolutions.DataAccessLayer;
 
 namespace PremierServiceSolutions.BusinessLogicLayer
 {
     class IndividualClientCallReportsBLL
     {
+        public List<Call> GetAllIndividualCallReports()
+        {
+            List<Call> data = new List<Call>();
+            try
+            {
+                IndividualClientCallReportsDAL reports = new IndividualClientCallReportsDAL();
+                data = reports.GetAllIndividualCallReports();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show($"Error on EmployeeBLL {e.Message}");
+            }
+            return data;
+        }
     }
 }
